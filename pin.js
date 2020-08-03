374e35b63ac3ef5735a520f7776752e796051607
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-08-03 17:46:10 +0600

    clear button done

--- pin.js
+++ pin.js
@@ -1,19 +1,32 @@
-const notifySection= document.getElementsByClassName("notify-section");
+const notifySection = document.getElementsByClassName("notify-section");
 notifySection[0].style.display = "none";
-document.querySelector('.generate-btn').addEventListener('click', function(){
+document.querySelector('.generate-btn').addEventListener('click', function () {
   var fourDigitRandom = Math.floor(1000 + Math.random() * 9000);
-   document.getElementsByClassName("form-control")[0].value = fourDigitRandom;
-    })
-   var button = document.getElementsByClassName("button");
-   var userPin = "";
-   var displayArea = document.getElementsByClassName("form-control")[1];
+  document.getElementsByClassName("form-control")[0].value = fourDigitRandom;
+})
+var button = document.getElementsByClassName("button");
+var userPin = "";
+var displayArea = document.getElementsByClassName("form-control")[1];
 
-   for( var i =0; i< button.length; i++){
-       button[i].addEventListener('click', function(){
-     userPin = userPin + this.id;
-     displayArea.value = userPin;
-        
-        })
-   }
-  
-      
+for (var i = 0; i < button.length; i++) {
+ 
+  if (button[i].id === "C") {
+    button[i].addEventListener('click',function(){
+      userPin = "";
+      displayArea.value = userPin;
+    })
+   
+  }
+  else {
+    button[i].addEventListener('click', function () {
+      console.log(this.id);
+      userPin = userPin + this.id;
+      displayArea.value = userPin;
+
+
+    })
+  }
+
+}
+
+
