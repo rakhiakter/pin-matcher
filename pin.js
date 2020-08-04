71447d1fa3dfe203a47963820b48118d746dab41
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-08-04 13:03:50 +0600

    submit button done

--- pin.js
+++ pin.js
@@ -1,5 +1,6 @@
 const notifySection = document.getElementsByClassName("notify-section");
 notifySection[0].style.display = "none";
+
 document.querySelector('.generate-btn').addEventListener('click', function () {
   var fourDigitRandom = Math.floor(1000 + Math.random() * 9000);
   document.getElementsByClassName("form-control")[0].value = fourDigitRandom;
@@ -9,17 +10,17 @@ var userPin = "";
 var displayArea = document.getElementsByClassName("form-control")[1];
 
 for (var i = 0; i < button.length; i++) {
- 
+
   if (button[i].id === "C") {
-    button[i].addEventListener('click',function(){
+    button[i].addEventListener('click', function () {
       userPin = "";
       displayArea.value = userPin;
     })
-   
+
   }
-  else if(button[i].id ==="<") {
-    button[i].addEventListener('click', function(){
-      userPin =userPin.substring(0,userPin.length-1);
+  else if (button[i].id === "<") {
+    button[i].addEventListener('click', function () {
+      userPin = userPin.substring(0, userPin.length - 1);
       displayArea.value = userPin;
     })
   }
@@ -32,11 +33,20 @@ for (var i = 0; i < button.length; i++) {
 
     })
   }
- 
+
 
 }
-var submitArea = document.getElementById("submit-button");
-submitArea.addEventListener('click', function(){
-  console.log(done);
+var submitArea = document.querySelector(".submit-btn");
+submitArea.addEventListener('click', function () {
+  console.log("done");
+  let fourDigitRandom =  document.getElementsByClassName("form-control")[0].value;
+  if (fourDigitRandom == userPin) {
+    let pinMatched = document.getElementsByClassName('notify');
+    pinMatched.style.display = "block";
+  }
+  else {
+    let tryAgain = document.getElementsByClassName('notify');
+    tryAgain.style.display = "block";
+  }
 })
 
